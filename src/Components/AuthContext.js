8053e264278c54df1f671c70adbbb255fc7d9a7f
Evanz-sys <v7713080@gmail.com>
2024-07-22 00:28:36 -0600

    homePage arreglado

--- src/Components/AuthContext.js
+++ src/Components/AuthContext.js
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/verify', { withCredentials: true } ,{
+        const response = await fetch('http://localhost:5000/api/verify',{
           method: 'GET',
           credentials: 'include',
         });
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,20 +5,20 @@ import "../css/Home.css";
 
 const Home = () => {
   const recipes = [
-    { id: 1, title: "Max", description: "Descripción de la receta 1", image: "" },
-    { id: 2, title: "Samu", description: "Descripción de la receta 2", image: "" },
-    { id: 3, title: "Dani", description: "Descripción de la receta 3", image: "" },
+    { id: 1, title: "Tortilla de Espinacas y Champiñones", description: " Es una opción perfecta para el desayuno, el almuerzo o la cena, proporcionando una buena dosis de proteínas y vegetales en cada bocado.", img: "/img/tortita.png" },
+    { id: 2, title: "Ensalada de Pollo y Aguacate", description: "Esta ensalada es ideal para una comida ligera, llena de proteínas y grasas saludables, y perfecta para disfrutar en cualquier época del año.", img: "/img/ensalada.png" },
+    { id: 3, title: "Pollo al Curry con Arroz de Coliflor", description: "Este plato es bajo en carbohidratos y lleno de sabor, ofreciendo una opción nutritiva y deliciosa para una comida principal.", img: "/img/pollo.png" },
   ];
 
   return (
     <div className="container mt-5">
       <div className="background-container">
-      <img src={process.env.PUBLIC_URL + "/HomePage.jpg"} alt="Background" className="background-image" />
-      <div className="background-content">
+        <img src={process.env.PUBLIC_URL + "/HomePage.jpg"} alt="Background" className="background-image" />
+        <div className="background-content">
           <h1>Cocina de Mercado</h1>
         </div>
       </div>
-      
+
       <p>¡Guarda tus recetas favoritas y compartelas!.</p>
 
       <Carousel>
@@ -26,7 +26,7 @@ const Home = () => {
           <Carousel.Item key={recipe.id}>
             <img
               className="d-block w-100"
-              src={recipe.image}
+              src={process.env.PUBLIC_URL + recipe.img}
               alt={recipe.title}
             />
             <Carousel.Caption>
